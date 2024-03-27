573e39d16efa6422d673c844c3a72aaf6ee7b207
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-03-27 21:38:45 +0900

    feat: add navbar component

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      'main-green-color': '#5E8D5A',
+    },
+  },
+  plugins: [],
+};
