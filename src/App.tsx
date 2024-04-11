14d6964ee73ae45c86e050a7539eb8dd7fb00da7
nnayoung <106735471+nnayoung@users.noreply.github.com>
2024-04-11 20:35:49 +0900

    feat: add maketrip page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import * as React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Main from './pages/main';
 import NavBar from './components/navbar';
+import MakeTrip from "./pages/MakeTrip";
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Main />}></Route>
+        <Route path="/" element={<Main />} />
+          <Route path="/maketrip" element={<MakeTrip/>} />
       </Routes>
     </BrowserRouter>
   );
