bbafbd22517fd733dfbbc36871c2a52794f04852
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-06-04 15:15:26 +0900

    fix: 일정 조회 연필 아이콘 추가가

--- src/components/MyPlanDetailBox.tsx
+++ src/components/MyPlanDetailBox.tsx
@@ -32,36 +32,42 @@ const MyPlanDetailBox: React.FC<PlanDetailBoxProps> = ({
     });
   };
 
-  const { time, locationName, content, imageUrl } = scheduleData;
+  const { time, locationName, title, content, imageUrl } = scheduleData;
 
   return (
     <div
       className="w-full h-[15%] p-5 flex rounded-md shadow-xl mb-5"
-      onClick={toDiaryDetail}
     >
-      <div className="w-full flex items-center">
-        <div className="w-[10%] font-['BMJUA'] text-[#FF9A9A] text-xl">
+      <div className="w-full flex items-center gap-4">
+
+        <img src="icon-pencil.png" alt="일기 수정"
+          className="h-5 w-5 cursor-pointer"
+          onClick={toDiaryDetail}/>
+        <div className="w-fit flex flex-col justify-center items-start font-['BMJUA'] text-[#FF9A9A] text-xl">
           {time}
+          <span className="font-['BMJUA'] text-black text-md">{locationName.length > 6 ? `${locationName.slice(0, 5)}...` : locationName}</span>
         </div>
-        <div className="w-[22%] font-['BMJUA'] text-2xl">{locationName.length > 7 ? `${locationName.slice(0, 6)}...` : locationName}</div>
-        <div className="w-[43%] font-['Nanum Gothic'] pr-2">{content}</div>
+        <div className="w-full flex flex-1 font-['Nanum Gothic'] pr-2">{content}</div>
         <div className="w-[25%]">
           {imageUrl ? (
-              <img
-                  src={imageUrl}
-                  width="250px"
-                  alt="일기사진"
-              />
+            <img
+              src={imageUrl}
+              width="250px"
+              alt="일기사진"
+            />
           ) : (
-              <div
-                  className="w-[100%] h-[60px] border-2 text-center flex items-center justify-center">
-              No Image
+            <div className="w-[100%] h-[60px] flex items-center justify-center">
+              <div className="text-center text-main-green-color font-bold font-BMJUA">
+                {title || content ? '' : '일기가 없습니다.'}
               </div>
-          )}
+            </div>
+          )
+          }
+
 
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
