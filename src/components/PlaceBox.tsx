60954481505c7f1f7c6ba547daa5e8c927a795e4
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-06-10 07:45:26 +0900

    feat: makeplan에서 장소 상세보기 추가

--- src/components/PlaceBox.tsx
+++ src/components/PlaceBox.tsx
@@ -1,18 +1,31 @@
 import '../index.css';
-import React from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 import Place from "../../types/Place";
 
 interface CityBoxProps {
     place: Place;
     addSelectedPlace: (place: Place) => void;
+    setIsOpen: (React.Dispatch<React.SetStateAction<boolean>>);
+    setPlace: React.Dispatch<React.SetStateAction<Place>>;
 }
 
-const PlaceBox: React.FC<CityBoxProps> = ({place, addSelectedPlace}) => {
+
+const PlaceBox: React.FC<CityBoxProps> = ({ place, addSelectedPlace, setIsOpen, setPlace }) => {
+
+
     const handleSelectPlace = () => {
         addSelectedPlace(place);
     };
+
+  const handleOpenModal = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setPlace(place);
+    setIsOpen(true);
+  }, []);
+
     return (
-        <div 
+        <div
             className="w-40 2xl:w-44 h-48 m-2 flex flex-col cursor-pointer rounded-lg overflow-hidden shadow-lg transform transition-transform hover:-translate-y-1 hover:shadow-xl"
             onClick={handleSelectPlace}
         >
@@ -32,7 +45,10 @@ const PlaceBox: React.FC<CityBoxProps> = ({place, addSelectedPlace}) => {
                 )}
             </div>
             <div className="w-full h-12 bg-black bg-opacity-60 flex items-center justify-center text-white text-lg font-bold p-2 border-t  font-['BMHANNApro'] border-gray-200">
-                {place.name.length > 7 ? `${place.name.slice(0, 6)}...` : place.name}
+                <div className="w-full h-12 flex items-center justify-center text-white text-lg font-bold p-2 border-t  font-['BMHANNApro'] border-gray-200">
+                    {place.name.length > 7 ? `${place.name.slice(0, 6)}...` : place.name}
+                </div>
+                <img src="search.png" alt="search" className="w-5 h-5" onClick={(e) => handleOpenModal(e)}/>
             </div>
         </div>
     );
--- src/pages/PlanDetail.tsx
+++ src/pages/PlanDetail.tsx
@@ -193,7 +193,7 @@ const PlanDetail: React.FC = () => {
 
   return (
     <div className="flex w-full h-[90%]">
-      <div className="w-1/2 h-full">
+      <div className="w-3/5 2xl:w-1/2 h-full">
         <div className="flex w-full h-[10%]">
           <i
             className="backArrow ml-2 cursor-pointer w-[10%]"
