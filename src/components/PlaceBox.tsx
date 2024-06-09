9d3aee35f9e1d606e1272b182cdb3986f2aa06c0
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-06-10 04:37:43 +0900

    fix: 장소 카드 디자인 수정

--- src/components/PlaceBox.tsx
+++ src/components/PlaceBox.tsx
@@ -12,20 +12,26 @@ const PlaceBox: React.FC<CityBoxProps> = ({place, addSelectedPlace}) => {
         addSelectedPlace(place);
     };
     return (
-        <div className="w-40 h-40 relative flex flex-col cursor-pointer" onClick={handleSelectPlace}>
-            {place.imageUrl ? (
-                <img
-                    src={place.imageUrl}
-                    alt={place.name}
-                    className="rounded-4 w-full h-full overflow-hidden object-cover"
-                />
-            ) : (
-                <img src={process.env.PUBLIC_URL + '/image/logo.png'} className='h-full w-full'></img>
-            )}
-            <div
-                className="absolute bottom-0 right-0 text-2xl font-['BMHANNApro'] text-white bg-black bg-opacity-50 p-2 rounded-tl rounded-br z-10"
-                style={{overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis"}}
-            >
+        <div 
+            className="w-40 2xl:w-44 h-48 m-2 flex flex-col cursor-pointer rounded-lg overflow-hidden shadow-lg transform transition-transform hover:-translate-y-1 hover:shadow-xl"
+            onClick={handleSelectPlace}
+        >
+            <div className="w-full h-36">
+                {place.imageUrl ? (
+                    <img
+                        src={place.imageUrl}
+                        alt={place.name}
+                        className="w-full h-full max-h-36 object-cover"
+                    />
+                ) : (
+                    <img
+                        src={process.env.PUBLIC_URL + '/image/logo.png'}
+                        alt="default"
+                        className="w-full h-full object-cover"
+                    />
+                )}
+            </div>
+            <div className="w-full h-12 bg-black bg-opacity-60 flex items-center justify-center text-white text-lg font-bold p-2 border-t  font-['BMHANNApro'] border-gray-200">
                 {place.name.length > 7 ? `${place.name.slice(0, 6)}...` : place.name}
             </div>
         </div>
