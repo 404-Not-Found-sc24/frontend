3d1471aef4e70c62e97fd97b881024676757e75f
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-06-10 08:20:29 +0900

    feat: makeplan 모달 디자인 수정

--- src/components/ModalBig.tsx
+++ src/components/ModalBig.tsx
@@ -9,7 +9,7 @@ const ModalBig: React.FC<ModalProps> = ({ onClose, children }) => {
     return (
         <div className="modal h-full">
             <div className="bpopup popBig w-4/5 h-[90%]">
-                <div className="">
+                <div className="w-full h-full">
                     {children}
                 </div>
                 <button className="popClose" onClick={onClose}></button>
